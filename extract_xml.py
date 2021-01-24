95038c4edf3c06ba63765da375101e03e8ddbd22
dimitrs <stoyan.dimitrov@dtms.de>
2021-01-24 18:46:06 +0100

    create connection to MySQL, parse the xml. requirements.txt

--- extract_xml.py
+++ extract_xml.py
@@ -1,4 +1,9 @@
 import requests
+import mysql.connector
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import tostring
+from mysql.connector import connect, Error, DatabaseError
+
 
 def wikimedia_request(search_term):
     """search for content on wikipedia pages
@@ -21,4 +26,40 @@ def wikimedia_request(search_term):
     R = S.get(url=URL, params=PARAMS)
     return R.text
 
-print(wikimedia_request('matrix'))
+# print(wikimedia_request('matrix'))
+
+try:
+    # create database wiki_search_results
+    connection = connect(
+        host="localhost",
+        user="root",
+        # user=input("Enter username: "),
+        # password=getpass("Enter password: "),
+    )
+    create_db = "CREATE DATABASE wiki_search_results"
+    cursor = connection.cursor()
+    cursor.execute(create_db)
+except DatabaseError as e:
+    print(e)
+    # connect if wiki_search_results exists already
+    connection = connect(
+        host="localhost",
+        user="root",
+        database="wiki_search_results",
+        # user=input("Enter username: "),
+        # password=getpass("Enter password: "),
+    )
+    cursor = connection.cursor()
+except Error as e:
+    print(e)
+
+# write the xml as string:
+tree = ET.parse("data/sample.xml")
+root = tree.getroot()
+# read the element 'p' (at the xml it's the search results)
+for search in root.iter("p"):
+    print(search.attrib.keys())
+
+
+
+connection.close()
